36686ba79986499693912e59a0d4bc6433097810
emireminovic <emir.eminovic45@gmail.com>
2026-09-15 08:48:49 +0200

    Add request stats

--- src/app/components/servisni-zahtevi/servisni-zahtevi.ts
+++ src/app/components/servisni-zahtevi/servisni-zahtevi.ts
@@ -18,6 +18,11 @@ export class ServisniZahtevi implements OnInit {
   serviseri: User[] = [];
   showModal = false;
   filterStatus = '';
+  totalZahteva = 0;
+  prijavljeno = 0;
+  uRadu = 0;
+  reseno = 0;
+  fakturisano = 0;
   newZahtev = { opis_kvara: '', uredjaj_id: 0, serviser_id: null as number | null };
 
   constructor(private api: ApiService, public auth: AuthService) {}
@@ -30,12 +35,29 @@ export class ServisniZahtevi implements OnInit {
 
   loadZahtevi() {
     if (this.auth.hasRole('klijent')) {
-      this.api.getZahtevi(this.filterStatus || undefined, undefined, this.auth.userId).subscribe(d => this.zahtevi = d);
+      this.api.getZahtevi(this.filterStatus || undefined, undefined, this.auth.userId).subscribe(d => {
+        this.zahtevi = d;
+        this.racunajStatistiku(d);
+      });
     } else if (this.auth.hasRole('serviser')) {
-      this.api.getZahtevi(this.filterStatus || undefined, this.auth.userId).subscribe(d => this.zahtevi = d);
+      this.api.getZahtevi(this.filterStatus || undefined, this.auth.userId).subscribe(d => {
+        this.zahtevi = d;
+        this.racunajStatistiku(d);
+      });
     } else {
-      this.api.getZahtevi(this.filterStatus || undefined).subscribe(d => this.zahtevi = d);
+      this.api.getZahtevi(this.filterStatus || undefined).subscribe(d => {
+        this.zahtevi = d;
+        this.racunajStatistiku(d);
+      });
     }
+  }
+
+  racunajStatistiku(d: ServisniZahtev[]) {
+    this.totalZahteva = d.length;
+    this.prijavljeno = d.filter(z => z.status === 'prijavljeno').length;
+    this.uRadu = d.filter(z => z.status === 'u radu').length;
+    this.reseno = d.filter(z => z.status === 'rešeno').length;
+    this.fakturisano = d.filter(z => z.status === 'fakturisano').length;
   }
 
   statusClass(status: string): string {
--- src/app/components/uredjaji/uredjaji.ts
+++ src/app/components/uredjaji/uredjaji.ts
@@ -17,6 +17,11 @@ export class Uredjaji implements OnInit {
   klijenti: User[] = [];
   showModal = false;
   filterTip = '';
+  totalUredjaja = 0;
+  aktivno = 0;
+  uServisu = 0;
+  neispravno = 0;
+  zamenjeno = 0;
   newUredjaj = { tip: 'fiskalna kasa' as TipUredjaja, serijski_broj: '', datum_instalacije: '', garancija_do: '', proizvodjac: '', verzija_softvera: '', klijent_id: 0 };
 
   constructor(private api: ApiService, public auth: AuthService) {}
@@ -28,10 +33,24 @@ export class Uredjaji implements OnInit {
 
   loadData() {
     if (this.auth.hasRole('klijent')) {
-      this.api.getUredjaji(this.filterTip || undefined, this.auth.userId).subscribe(d => this.uredjaji = d);
+      this.api.getUredjaji(this.filterTip || undefined, this.auth.userId).subscribe(d => {
+        this.uredjaji = d;
+        this.racunajStatistiku(d);
+      });
     } else {
-      this.api.getUredjaji(this.filterTip || undefined).subscribe(d => this.uredjaji = d);
+      this.api.getUredjaji(this.filterTip || undefined).subscribe(d => {
+        this.uredjaji = d;
+        this.racunajStatistiku(d);
+      });
     }
+  }
+
+  racunajStatistiku(d: Uredjaj[]) {
+    this.totalUredjaja = d.length;
+    this.aktivno = d.filter(u => u.status === 'aktivno').length;
+    this.uServisu = d.filter(u => u.status === 'u servisu').length;
+    this.neispravno = d.filter(u => u.status === 'neispravno').length;
+    this.zamenjeno = d.filter(u => u.status === 'zamenjeno').length;
   }
 
   statusClass(s: string): string {
